dff20701713ff28e521c2754a11166e9475e1a33
alexsnitol <sslotin74@gmail.com>
2022-03-01 18:55:34 +0300

    fix relative path to html

--- scripts/includeHTML.js
+++ scripts/includeHTML.js
@@ -1,41 +1,52 @@
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
+    let path = document.location.pathname;
+    var z, i, elmnt, file, xhttp, href;
     /*loop through a collection of all HTML elements:*/
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
         elmnt = z[i];
         /*search for elements with a certain atrribute:*/
         file = elmnt.getAttribute("include-html");
-        if (file) {
-        /*make an HTTP request using the attribute value as the file name:*/
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4) {
-                if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                /*remove the attribute, and call this function once more:*/
-                elmnt.removeAttribute("include-html");
-                includeHTML();
+        href = elmnt.getAttribute("href");
+
+        if (path.substring(0, 30) === "/INERI_website/html/vacancies/") {
+            if (href && elmnt.tagName === "A") {
+                if (elmnt.getAttribute("href").substring(0, 3) !== "http" && elmnt.getAttribute("href").substring(0, 3) !== "../") {
+                    elmnt.setAttribute("href", "../" + elmnt.getAttribute("href"));
+                }
             }
         }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /*exit the function:*/
-        return;
+
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                    /*remove the attribute, and call this function once more:*/
+                    elmnt.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /*exit the function:*/
+            return;
         }
     }
 
-    let pn = document.location.pathname;
+    let page = path.split("/").pop();
 
-    if (pn === "/INERI_website/html/index.html") {
+    if (page === "index.html") {
         document.getElementById("menu-main").classList.add("current");
         document.getElementById("menu-main-mini").classList.add("current");
     }
-    else if (pn === "/INERI_website/html/contacts.html") {
+    else if (page === "contacts.html") {
         document.getElementById("menu-contacts").classList.add("current");
         document.getElementById("menu-contacts-mini").classList.add("current");
     }
-    else if (pn === "/INERI_website/html/vacancies.html") {
+    else if (page === "vacancies.html") {
         document.getElementById("menu-vacancies").classList.add("current");
         document.getElementById("menu-vacancies-mini").classList.add("current");
     }
